21903e8363d7bedfa4cd3d5d2a0c6def42d996c1
Marcel Graupera <marcel.graupera@quantum.com>
2024-03-28 14:46:31 +0100

    Add api/token endpoint to get token

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['auth:sanctum'])->get('/token', function (Request $request) {
+    $token = $request->user()->createToken('quantum-api-token');
+ 
+    return ['token' => $token->plainTextToken];
+});
